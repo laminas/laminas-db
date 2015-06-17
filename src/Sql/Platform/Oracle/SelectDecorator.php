17867ca74b1c3c956feb05b5b13bdbb78f11cb16
Romulo Busatto <busatto.romulo@gmail.com>
2015-06-17 15:47:27 -0300

    Update SelectDecorator.php
    Ajusted problem with limit and offset in subselects

--- src/Sql/Platform/Oracle/SelectDecorator.php
+++ src/Sql/Platform/Oracle/SelectDecorator.php
@@ -65,7 +65,9 @@ class SelectDecorator extends Select implements PlatformDecoratorInterface
 
         $starSuffix = $platform->getIdentifierSeparator() . self::SQL_STAR;
         foreach ($selectParameters[0] as $i => $columnParameters) {
-            if ($columnParameters[0] == self::SQL_STAR || (isset($columnParameters[1]) && $columnParameters[1] == self::SQL_STAR) || strpos($columnParameters[0], $starSuffix)) {
+            if ($columnParameters[0] == self::SQL_STAR ||
+                (isset($columnParameters[1]) && $columnParameters[1] == self::SQL_STAR) ||
+                strpos($columnParameters[0], $starSuffix)) {
                 $selectParameters[0] = [[self::SQL_STAR]];
                 break;
             }
@@ -81,32 +83,26 @@ class SelectDecorator extends Select implements PlatformDecoratorInterface
 
         // first, produce column list without compound names (using the AS portion only)
         array_unshift($sqls, $this->createSqlFromSpecificationAndParameters(
-            ['SELECT %1$s FROM (SELECT b.%1$s, rownum b_rownum FROM (' => current($this->specifications[self::SELECT])], $selectParameters
+            array('SELECT %1$s FROM (SELECT b.%1$s, rownum b_rownum FROM (' => current($this->specifications[self::SELECT])), $selectParameters
         ));
 
         if ($parameterContainer) {
+            $number = $this->processInfo['subselectCount'] ? $this->processInfo['subselectCount'] : '';
             if ($this->limit === null) {
-                array_push($sqls, ') b ) WHERE b_rownum > (:offset)');
-                $parameterContainer->offsetSet('offset', $this->offset, $parameterContainer::TYPE_INTEGER);
+                array_push($sqls, ') b ) WHERE b_rownum > (:offset'.$number.')');
+                $parameterContainer->offsetSet('offset'.$number, $this->offset, $parameterContainer::TYPE_INTEGER);
             } else {
                 // create bottom part of query, with offset and limit using row_number
-                array_push($sqls, ') b WHERE rownum <= (:offset+:limit)) WHERE b_rownum >= (:offset + 1)');
-                $parameterContainer->offsetSet('offset', $this->offset, $parameterContainer::TYPE_INTEGER);
-                $parameterContainer->offsetSet('limit', $this->limit, $parameterContainer::TYPE_INTEGER);
+                array_push($sqls, ') b WHERE rownum <= (:offset'.$number.'+:limit'.$number.')) WHERE b_rownum >= (:offset'.$number.' + 1)');
+                $parameterContainer->offsetSet('offset'.$number, $this->offset, $parameterContainer::TYPE_INTEGER);
+                $parameterContainer->offsetSet('limit'.$number, $this->limit, $parameterContainer::TYPE_INTEGER);
             }
+            $this->processInfo['subselectCount']++;
         } else {
             if ($this->limit === null) {
-                array_push($sqls, ') b ) WHERE b_rownum > ('. (int) $this->offset. ')'
-                );
+                array_push($sqls, ') b ) WHERE b_rownum > ('. (int) $this->offset. ')');
             } else {
-                array_push($sqls, ') b WHERE rownum <= ('
-                        . (int) $this->offset
-                        . '+'
-                        . (int) $this->limit
-                        . ')) WHERE b_rownum >= ('
-                        . (int) $this->offset
-                        . ' + 1)'
-                );
+                array_push($sqls, ') b WHERE rownum <= ('. (int) $this->offset. '+'. (int) $this->limit. ')) WHERE b_rownum >= ('. (int) $this->offset. ' + 1)');
             }
         }
 
