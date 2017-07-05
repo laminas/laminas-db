94714726b0c4bd904aff501806b39df221b33040
Bart McLeod <mcleod@spaceweb.nl>
2017-07-05 12:35:35 +0200

    simplified comment, without the bloat generation by the IDE

--- test/ResultSet/ReturnType.php
+++ test/ResultSet/ReturnType.php
@@ -4,9 +4,6 @@ namespace ZendTest\Db\ResultSet;
 /**
  * This class is just here to be able to provide the simplest possible custom return type
  * for a ResultSet when calling ResultSet::setArrayObjectPrototype or using the constructor argument.
- *
- * Class ReturnType
- * @package ZendTest\Db\ResultSet
  */
 class ReturnType
 {
