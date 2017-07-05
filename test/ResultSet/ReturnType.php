634782e97c47cf42087a1291c492cd7f7614ecc0
Bart McLeod <mcleod@spaceweb.nl>
2017-07-05 11:55:55 +0200

    Removed whitespace and added comment to illustrate the purpose of ReturnType test class

--- test/ResultSet/ReturnType.php
+++ test/ResultSet/ReturnType.php
@@ -1,13 +1,16 @@
 <?php
-
-
 namespace ZendTest\Db\ResultSet;
 
-
+/**
+ * This class is just here to be able to provide the simplest possible custom return type
+ * for a ResultSet when calling ResultSet::setArrayObjectPrototype or using the constructor argument.
+ *
+ * Class ReturnType
+ * @package ZendTest\Db\ResultSet
+ */
 class ReturnType
 {
     public function exchangeArray(array $values)
     {
-
     }
 }
