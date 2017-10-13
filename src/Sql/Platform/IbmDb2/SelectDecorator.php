ea15efa5c1c8af12a77e2de95095cc6433c34db8
Rob Allen <rob@akrabat.com>
2017-10-13 20:57:51 +0100

    Add support for LIMIT OFFSET for db2

--- src/Sql/Platform/IbmDb2/SelectDecorator.php
+++ src/Sql/Platform/IbmDb2/SelectDecorator.php
@@ -27,7 +27,13 @@ class SelectDecorator extends Select implements PlatformDecoratorInterface
      */
     protected $subject = null;
 
-    /**
+     /**
+     * @var bool
+     */
+    protected $supportsLimitOffset = false;
+
+
+   /**
      * @return bool
      */
     public function getIsSelectContainDistinct()
@@ -49,6 +55,22 @@ class SelectDecorator extends Select implements PlatformDecoratorInterface
     public function setSubject($select)
     {
         $this->subject = $select;
+    }
+
+    /**
+     * @return bool
+     */
+    public function getSupportsLimitOffset()
+    {
+        return $this->supportsLimitOffset;
+    }
+
+    /**
+     * @param bool $supportsLimitOffset
+     */
+    public function setSupportsLimitOffset($supportsLimitOffset)
+    {
+        $this->supportsLimitOffset = $supportsLimitOffset;
     }
 
     /**
@@ -79,6 +101,20 @@ class SelectDecorator extends Select implements PlatformDecoratorInterface
     protected function processLimitOffset(PlatformInterface $platform, DriverInterface $driver = null, ParameterContainer $parameterContainer = null, &$sqls, &$parameters)
     {
         if ($this->limit === null && $this->offset === null) {
+            return;
+        }
+
+        if ($this->supportsLimitOffset) {
+            // Note: db2_prepare/db2_execute fails with positional parameters, for LIMIT & OFFSET
+            $limit = (int)$this->limit;
+            $offset = (int)$this->offset;
+            if ($this->limit) {
+                if ($this->offset) {
+                    array_push($sqls, sprintf("LIMIT %s OFFSET %s", $limit, $offset));
+                } else {
+                    array_push($sqls, sprintf("LIMIT %s", $limit));
+                }
+            }
             return;
         }
 
