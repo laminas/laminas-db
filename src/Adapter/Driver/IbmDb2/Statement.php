984e3553afb8f9b928a8e951ea69bf658b0a46c6
Rob Allen <rob@akrabat.com>
2017-10-20 08:56:00 +0100

    Fix cs-check error

--- src/Adapter/Driver/IbmDb2/Statement.php
+++ src/Adapter/Driver/IbmDb2/Statement.php
@@ -10,7 +10,6 @@
 namespace Zend\Db\Adapter\Driver\IbmDb2;
 
 use ErrorException;
-use Throwable;
 use Zend\Db\Adapter\Driver\StatementInterface;
 use Zend\Db\Adapter\Exception;
 use Zend\Db\Adapter\ParameterContainer;
