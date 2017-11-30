366661f4c6e7874946f2254535f5010b79e9b5ec
webimpress <contact@webimpress.com>
2017-11-30 13:03:41 +0000

    Removed unused condition part
    It is not possible to have there Countable element.
    Please note it was not even imported so it was not working at all.

--- src/Sql/AbstractSql.php
+++ src/Sql/AbstractSql.php
@@ -241,7 +241,7 @@ abstract class AbstractSql implements SqlInterface
             if (isset($paramSpecs[$position]['combinedby'])) {
                 $multiParamValues = [];
                 foreach ($paramsForPosition as $multiParamsForPosition) {
-                    if (is_array($multiParamsForPosition) || $multiParamsForPosition instanceof Countable) {
+                    if (is_array($multiParamsForPosition)) {
                         $ppCount = count($multiParamsForPosition);
                     } else {
                         $ppCount = 1;
