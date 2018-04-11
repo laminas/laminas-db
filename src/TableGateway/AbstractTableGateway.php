96a86326a5c1c6825c04abcaf5cd036627b49629
lowtower <lowtower@users.noreply.github.com>
2018-04-11 13:50:05 +0200

    Add missing use statement

--- src/TableGateway/AbstractTableGateway.php
+++ src/TableGateway/AbstractTableGateway.php
@@ -10,6 +10,7 @@
 namespace Zend\Db\TableGateway;
 
 use Zend\Db\Adapter\AdapterInterface;
+use Zend\Db\ResultSet\AbstractResultSet;
 use Zend\Db\ResultSet\ResultSet;
 use Zend\Db\ResultSet\ResultSetInterface;
 use Zend\Db\Sql\Delete;
