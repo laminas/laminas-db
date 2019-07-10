070c16ee12c9e8f58855c05fa02330f804817151
Jasper van Gestel <jvangestel@gmail.com>
2019-07-10 09:13:41 +0200

    fixed unit tests for pdo_mysql

--- test/integration/TestFixtures/mysql.sql
+++ test/integration/TestFixtures/mysql.sql
@@ -9,7 +9,7 @@ INSERT INTO test (name, value) VALUES
 ('foo', 'bar'),
 ('bar', 'baz'),
 ('123a', 'bar'),
-('123', 'baz');
+('123', 'bar');
 
 CREATE TABLE IF NOT EXISTS test_charset (
     id INT NOT NULL AUTO_INCREMENT,
