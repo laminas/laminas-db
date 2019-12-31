540b861ad2569618fcb82a67a0bec4ca10e01b03
Matthew Weier O'Phinney <matthew@weierophinney.net>
2019-12-31 13:59:31 -0600

    Rewriting as Laminas Project package

--- test/Sql/Predicate/LiteralTest.php
+++ test/Sql/Predicate/LiteralTest.php
@@ -1,15 +1,14 @@
 <?php
+
 /**
- * Zend Framework (http://framework.zend.com/)
- *
- * @link      http://github.com/zendframework/zf2 for the canonical source repository
- * @copyright Copyright (c) 2005-2015 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   http://framework.zend.com/license/new-bsd New BSD License
+ * @see       https://github.com/laminas/laminas-db for the canonical source repository
+ * @copyright https://github.com/laminas/laminas-db/blob/master/COPYRIGHT.md
+ * @license   https://github.com/laminas/laminas-db/blob/master/LICENSE.md New BSD License
  */
 
-namespace ZendTest\Db\Sql\Predicate;
+namespace LaminasTest\Db\Sql\Predicate;
 
-use Zend\Db\Sql\Predicate\Literal;
+use Laminas\Db\Sql\Predicate\Literal;
 
 class LiteralTest extends \PHPUnit_Framework_TestCase
 {
