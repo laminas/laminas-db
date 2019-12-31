5cf199a46e8820a528ea05869deb976d026f827c
Matthew Weier O'Phinney <matthew@weierophinney.net>
2019-12-31 13:58:05 -0600

    Rewriting as Laminas Project package

--- src/Adapter/Profiler/ProfilerInterface.php
+++ src/Adapter/Profiler/ProfilerInterface.php
@@ -1,18 +1,17 @@
 <?php
+
 /**
- * Zend Framework (http://framework.zend.com/)
- *
- * @link      http://github.com/zendframework/zf2 for the canonical source repository
- * @copyright Copyright (c) 2005-2015 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   http://framework.zend.com/license/new-bsd New BSD License
+ * @see       https://github.com/laminas/laminas-db for the canonical source repository
+ * @copyright https://github.com/laminas/laminas-db/blob/master/COPYRIGHT.md
+ * @license   https://github.com/laminas/laminas-db/blob/master/LICENSE.md New BSD License
  */
 
-namespace Zend\Db\Adapter\Profiler;
+namespace Laminas\Db\Adapter\Profiler;
 
 interface ProfilerInterface
 {
     /**
-     * @param string|\Zend\Db\Adapter\StatementContainerInterface $target
+     * @param string|\Laminas\Db\Adapter\StatementContainerInterface $target
      * @return mixed
      */
     public function profilerStart($target);
