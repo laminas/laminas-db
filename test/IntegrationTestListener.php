68c7600de60a8bedb85e09448235d4a16d5039f2
Matthew Weier O'Phinney <matthew@weierophinney.net>
2019-12-31 13:58:20 -0600

    Rewriting as Laminas Project package

--- test/IntegrationTestListener.php
+++ test/IntegrationTestListener.php
@@ -1,13 +1,12 @@
 <?php
+
 /**
- * Zend Framework (http://framework.zend.com/)
- *
- * @link      http://github.com/zendframework/zf2 for the canonical source repository
- * @copyright Copyright (c) 2005-2015 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   http://framework.zend.com/license/new-bsd New BSD License
+ * @see       https://github.com/laminas/laminas-db for the canonical source repository
+ * @copyright https://github.com/laminas/laminas-db/blob/master/COPYRIGHT.md
+ * @license   https://github.com/laminas/laminas-db/blob/master/LICENSE.md New BSD License
  */
 
-namespace ZendTest\Db;
+namespace LaminasTest\Db;
 
 use Exception;
 use PHPUnit_Framework_AssertionFailedError;
@@ -29,56 +28,56 @@ class IntegrationTestListener implements PHPUnit_Framework_TestListener
 
     public function __construct()
     {
-        if (isset($GLOBALS['ZEND_DB_ADAPTER_DRIVER_MYSQL_HOSTNAME'])) {
+        if (isset($GLOBALS['LAMINAS_DB_ADAPTER_DRIVER_MYSQL_HOSTNAME'])) {
             if (extension_loaded('mysqli')) {
                 $this->adapters['mysqli'] = new \mysqli(
-                    $GLOBALS['ZEND_DB_ADAPTER_DRIVER_MYSQL_HOSTNAME'],
-                    $GLOBALS['ZEND_DB_ADAPTER_DRIVER_MYSQL_USERNAME'],
-                    $GLOBALS['ZEND_DB_ADAPTER_DRIVER_MYSQL_PASSWORD'],
-                    $GLOBALS['ZEND_DB_ADAPTER_DRIVER_MYSQL_DATABASE']
+                    $GLOBALS['LAMINAS_DB_ADAPTER_DRIVER_MYSQL_HOSTNAME'],
+                    $GLOBALS['LAMINAS_DB_ADAPTER_DRIVER_MYSQL_USERNAME'],
+                    $GLOBALS['LAMINAS_DB_ADAPTER_DRIVER_MYSQL_PASSWORD'],
+                    $GLOBALS['LAMINAS_DB_ADAPTER_DRIVER_MYSQL_DATABASE']
                 );
             }
             if (extension_loaded('pdo')) {
                 $this->adapters['pdo_mysql'] = new \Pdo(
-                    'mysql:host=' . $GLOBALS['ZEND_DB_ADAPTER_DRIVER_MYSQL_HOSTNAME'] . ';dbname=' . $GLOBALS['ZEND_DB_ADAPTER_DRIVER_MYSQL_DATABASE'],
-                    $GLOBALS['ZEND_DB_ADAPTER_DRIVER_MYSQL_USERNAME'],
-                    $GLOBALS['ZEND_DB_ADAPTER_DRIVER_MYSQL_PASSWORD']
+                    'mysql:host=' . $GLOBALS['LAMINAS_DB_ADAPTER_DRIVER_MYSQL_HOSTNAME'] . ';dbname=' . $GLOBALS['LAMINAS_DB_ADAPTER_DRIVER_MYSQL_DATABASE'],
+                    $GLOBALS['LAMINAS_DB_ADAPTER_DRIVER_MYSQL_USERNAME'],
+                    $GLOBALS['LAMINAS_DB_ADAPTER_DRIVER_MYSQL_PASSWORD']
                 );
             }
         }
-        if (isset($GLOBALS['ZEND_DB_ADAPTER_DRIVER_PGSQL_HOSTNAME'])) {
+        if (isset($GLOBALS['LAMINAS_DB_ADAPTER_DRIVER_PGSQL_HOSTNAME'])) {
             if (extension_loaded('pgsql')) {
                 $this->adapters['pgsql'] = pg_connect(
-                    'host=' . $GLOBALS['ZEND_DB_ADAPTER_DRIVER_PGSQL_HOSTNAME']
-                        . ' dbname=' . $GLOBALS['ZEND_DB_ADAPTER_DRIVER_PGSQL_DATABASE']
-                        . ' user=' . $GLOBALS['ZEND_DB_ADAPTER_DRIVER_PGSQL_USERNAME']
-                        . ' password=' . $GLOBALS['ZEND_DB_ADAPTER_DRIVER_PGSQL_PASSWORD']
+                    'host=' . $GLOBALS['LAMINAS_DB_ADAPTER_DRIVER_PGSQL_HOSTNAME']
+                        . ' dbname=' . $GLOBALS['LAMINAS_DB_ADAPTER_DRIVER_PGSQL_DATABASE']
+                        . ' user=' . $GLOBALS['LAMINAS_DB_ADAPTER_DRIVER_PGSQL_USERNAME']
+                        . ' password=' . $GLOBALS['LAMINAS_DB_ADAPTER_DRIVER_PGSQL_PASSWORD']
                 );
             }
             if (extension_loaded('pdo')) {
                 $this->adapters['pdo_pgsql'] = new \Pdo(
-                    'pgsql:host=' . $GLOBALS['ZEND_DB_ADAPTER_DRIVER_PGSQL_HOSTNAME'] . ';dbname=' . $GLOBALS['ZEND_DB_ADAPTER_DRIVER_PGSQL_DATABASE'],
-                    $GLOBALS['ZEND_DB_ADAPTER_DRIVER_PGSQL_USERNAME'],
-                    $GLOBALS['ZEND_DB_ADAPTER_DRIVER_PGSQL_PASSWORD']
+                    'pgsql:host=' . $GLOBALS['LAMINAS_DB_ADAPTER_DRIVER_PGSQL_HOSTNAME'] . ';dbname=' . $GLOBALS['LAMINAS_DB_ADAPTER_DRIVER_PGSQL_DATABASE'],
+                    $GLOBALS['LAMINAS_DB_ADAPTER_DRIVER_PGSQL_USERNAME'],
+                    $GLOBALS['LAMINAS_DB_ADAPTER_DRIVER_PGSQL_PASSWORD']
                 );
             }
         }
-        if (isset($GLOBALS['ZEND_DB_ADAPTER_DRIVER_SQLITE_MEMORY'])) {
+        if (isset($GLOBALS['LAMINAS_DB_ADAPTER_DRIVER_SQLITE_MEMORY'])) {
             if (extension_loaded('pdo')) {
                 $this->adapters['pdo_sqlite'] = new \Pdo(
                     'sqlite::memory:'
                 );
             }
         }
-        if (isset($GLOBALS['ZEND_DB_ADAPTER_DRIVER_SQLSRV_HOSTNAME'])) {
+        if (isset($GLOBALS['LAMINAS_DB_ADAPTER_DRIVER_SQLSRV_HOSTNAME'])) {
             if (extension_loaded('sqlsrv')) {
                 $this->adapters['sqlsrv'] = sqlsrv_connect(
-                    $GLOBALS['ZEND_DB_ADAPTER_DRIVER_SQLSRV_HOSTNAME'],
+                    $GLOBALS['LAMINAS_DB_ADAPTER_DRIVER_SQLSRV_HOSTNAME'],
                     array(
-                        'UID' => $GLOBALS['ZEND_DB_ADAPTER_DRIVER_SQLSRV_USERNAME'],
-                        'PWD' => $GLOBALS['ZEND_DB_ADAPTER_DRIVER_SQLSRV_PASSWORD'],
-                        'Database' => (isset($GLOBALS['ZEND_DB_ADAPTER_DRIVER_SQLSRV_DATABASE'])
-                            ? $GLOBALS['ZEND_DB_ADAPTER_DRIVER_SQLSRV_DATABASE'] : null)
+                        'UID' => $GLOBALS['LAMINAS_DB_ADAPTER_DRIVER_SQLSRV_USERNAME'],
+                        'PWD' => $GLOBALS['LAMINAS_DB_ADAPTER_DRIVER_SQLSRV_PASSWORD'],
+                        'Database' => (isset($GLOBALS['LAMINAS_DB_ADAPTER_DRIVER_SQLSRV_DATABASE'])
+                            ? $GLOBALS['LAMINAS_DB_ADAPTER_DRIVER_SQLSRV_DATABASE'] : null)
                     )
                 );
                 if (!$this->adapters['sqlsrv']) {
@@ -88,11 +87,11 @@ class IntegrationTestListener implements PHPUnit_Framework_TestListener
             }
             if (extension_loaded('pdo')) {
                 $this->adapters['pdo_sqlsrv'] = new \Pdo(
-                    'sqlsrv:Server=' . $GLOBALS['ZEND_DB_ADAPTER_DRIVER_SQLSRV_HOSTNAME']
-                        . ';Database=' . (isset($GLOBALS['ZEND_DB_ADAPTER_DRIVER_SQLSRV_DATABASE'])
-                            ? $GLOBALS['ZEND_DB_ADAPTER_DRIVER_SQLSRV_DATABASE'] : null),
-                    $GLOBALS['ZEND_DB_ADAPTER_DRIVER_SQLSRV_USERNAME'],
-                    $GLOBALS['ZEND_DB_ADAPTER_DRIVER_SQLSRV_PASSWORD']
+                    'sqlsrv:Server=' . $GLOBALS['LAMINAS_DB_ADAPTER_DRIVER_SQLSRV_HOSTNAME']
+                        . ';Database=' . (isset($GLOBALS['LAMINAS_DB_ADAPTER_DRIVER_SQLSRV_DATABASE'])
+                            ? $GLOBALS['LAMINAS_DB_ADAPTER_DRIVER_SQLSRV_DATABASE'] : null),
+                    $GLOBALS['LAMINAS_DB_ADAPTER_DRIVER_SQLSRV_USERNAME'],
+                    $GLOBALS['LAMINAS_DB_ADAPTER_DRIVER_SQLSRV_PASSWORD']
                 );
             }
         }
@@ -130,7 +129,7 @@ class IntegrationTestListener implements PHPUnit_Framework_TestListener
     {
         /** @var $test \PHPUnit_Framework_TestCase */
         $testcase = get_class($test);
-        if (strpos($testcase, 'ZendTest\Db') === 0 && strpos($testcase, 'Integration')) {
+        if (strpos($testcase, 'LaminasTest\Db') === 0 && strpos($testcase, 'Integration')) {
             $refObj = new \ReflectionObject($test);
             if ($refObj->hasProperty('adapters')) {
                 $refProp = $refObj->getProperty('adapters');
@@ -150,7 +149,7 @@ class IntegrationTestListener implements PHPUnit_Framework_TestListener
     {
         /** @var $test \PHPUnit_Framework_TestCase */
         $testcase = get_class($test);
-        if (strpos($testcase, 'ZendTest\Db') === 0 && strpos($testcase, 'Integration')) {
+        if (strpos($testcase, 'LaminasTest\Db') === 0 && strpos($testcase, 'Integration')) {
             $refObj = new \ReflectionObject($test);
             if ($refObj->hasProperty('adapters')) {
                 $refProp = $refObj->getProperty('adapters');
