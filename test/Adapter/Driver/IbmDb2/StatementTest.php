b199d53fc717dfcdfae922324ec531b6c0ccf6ff
Matthew Weier O'Phinney <matthew@weierophinney.net>
2019-12-31 13:59:11 -0600

    Rewriting as Laminas Project package

--- test/Adapter/Driver/IbmDb2/StatementTest.php
+++ test/Adapter/Driver/IbmDb2/StatementTest.php
@@ -1,17 +1,16 @@
 <?php
+
 /**
- * Zend Framework (http://framework.zend.com/)
- *
- * @link      http://github.com/zendframework/zf2 for the canonical source repository
- * @copyright Copyright (c) 2005-2015 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   http://framework.zend.com/license/new-bsd New BSD License
+ * @see       https://github.com/laminas/laminas-db for the canonical source repository
+ * @copyright https://github.com/laminas/laminas-db/blob/master/COPYRIGHT.md
+ * @license   https://github.com/laminas/laminas-db/blob/master/LICENSE.md New BSD License
  */
 
-namespace ZendTest\Db\Adapter\Driver\IbmDb2;
+namespace LaminasTest\Db\Adapter\Driver\IbmDb2;
 
-use Zend\Db\Adapter\Driver\IbmDb2\Statement;
-use Zend\Db\Adapter\Driver\IbmDb2\IbmDb2;
-use Zend\Db\Adapter\ParameterContainer;
+use Laminas\Db\Adapter\Driver\IbmDb2\IbmDb2;
+use Laminas\Db\Adapter\Driver\IbmDb2\Statement;
+use Laminas\Db\Adapter\ParameterContainer;
 
 class StatementTest extends \PHPUnit_Framework_TestCase
 {
@@ -38,7 +37,7 @@ class StatementTest extends \PHPUnit_Framework_TestCase
     }
 
     /**
-     * @covers Zend\Db\Adapter\Driver\IbmDb2\Statement::setDriver
+     * @covers Laminas\Db\Adapter\Driver\IbmDb2\Statement::setDriver
      */
     public function testSetDriver()
     {
@@ -46,7 +45,7 @@ class StatementTest extends \PHPUnit_Framework_TestCase
     }
 
     /**
-     * @covers Zend\Db\Adapter\Driver\IbmDb2\Statement::setParameterContainer
+     * @covers Laminas\Db\Adapter\Driver\IbmDb2\Statement::setParameterContainer
      */
     public function testSetParameterContainer()
     {
@@ -54,7 +53,7 @@ class StatementTest extends \PHPUnit_Framework_TestCase
     }
 
     /**
-     * @covers Zend\Db\Adapter\Driver\IbmDb2\Statement::getParameterContainer
+     * @covers Laminas\Db\Adapter\Driver\IbmDb2\Statement::getParameterContainer
      * @todo   Implement testGetParameterContainer().
      */
     public function testGetParameterContainer()
@@ -65,7 +64,7 @@ class StatementTest extends \PHPUnit_Framework_TestCase
     }
 
     /**
-     * @covers Zend\Db\Adapter\Driver\IbmDb2\Statement::getResource
+     * @covers Laminas\Db\Adapter\Driver\IbmDb2\Statement::getResource
      * @todo   Implement testGetResource().
      */
     public function testGetResource()
@@ -77,7 +76,7 @@ class StatementTest extends \PHPUnit_Framework_TestCase
     }
 
     /**
-     * @covers Zend\Db\Adapter\Driver\IbmDb2\Statement::setSql
+     * @covers Laminas\Db\Adapter\Driver\IbmDb2\Statement::setSql
      * @todo   Implement testSetSql().
      */
     public function testSetSql()
@@ -89,7 +88,7 @@ class StatementTest extends \PHPUnit_Framework_TestCase
     }
 
     /**
-     * @covers Zend\Db\Adapter\Driver\IbmDb2\Statement::getSql
+     * @covers Laminas\Db\Adapter\Driver\IbmDb2\Statement::getSql
      * @todo   Implement testGetSql().
      */
     public function testGetSql()
@@ -101,7 +100,7 @@ class StatementTest extends \PHPUnit_Framework_TestCase
     }
 
     /**
-     * @covers Zend\Db\Adapter\Driver\IbmDb2\Statement::prepare
+     * @covers Laminas\Db\Adapter\Driver\IbmDb2\Statement::prepare
      * @todo   Implement testPrepare().
      */
     public function testPrepare()
@@ -113,7 +112,7 @@ class StatementTest extends \PHPUnit_Framework_TestCase
     }
 
     /**
-     * @covers Zend\Db\Adapter\Driver\IbmDb2\Statement::isPrepared
+     * @covers Laminas\Db\Adapter\Driver\IbmDb2\Statement::isPrepared
      * @todo   Implement testIsPrepared().
      */
     public function testIsPrepared()
@@ -125,7 +124,7 @@ class StatementTest extends \PHPUnit_Framework_TestCase
     }
 
     /**
-     * @covers Zend\Db\Adapter\Driver\IbmDb2\Statement::execute
+     * @covers Laminas\Db\Adapter\Driver\IbmDb2\Statement::execute
      * @todo   Implement testExecute().
      */
     public function testExecute()
