45412a173c40617af55f8be79b27f14792015fb8
Matthew Weier O'Phinney <matthew@weierophinney.net>
2021-09-15 09:05:19 -0500

    qa: fix CI fixtures used by Vagrantfile
    Were erroneously referring to "zenddb" instead of "laminasdb".
    
    Signed-off-by: Matthew Weier O'Phinney <matthew@weierophinney.net>

--- .ci/mysql_fixtures.sh
+++ .ci/mysql_fixtures.sh
@@ -2,4 +2,4 @@
 
 echo "Configure MySQL test database"
 
-mysql --user=root --password=Password123 -e 'create database zenddb_test;'
+mysql --user=root --password=Password123 -e 'create database laminasdb_test;'
