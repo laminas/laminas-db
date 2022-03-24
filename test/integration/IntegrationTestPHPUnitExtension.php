a85bd9d2a1ff774fab892d335851cd6197a314b6
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2022-03-24 02:51:14 -0400

    Create IntegrationTestPHPUnitExtension.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ test/integration/IntegrationTestPHPUnitExtension.php
@@ -0,0 +1,86 @@
+<?php
+
+declare(strict_types=1);
+
+namespace LaminasIntegrationTest\Db;
+
+use LaminasIntegrationTest\Db\Platform\MysqlFixtureLoader;
+use LaminasIntegrationTest\Db\Platform\PgsqlFixtureLoader;
+use LaminasIntegrationTest\Db\Platform\SqlServerFixtureLoader;
+use PHPUnit\Runner\AfterLastTestHook;
+use PHPUnit\Runner\BeforeFirstTestHook;
+use PHPUnit\Runner\TestHook;
+
+use function array_search;
+use function getenv;
+use function printf;
+use function trim;
+
+class IntegrationTestPHPUnitExtension implements TestHook, BeforeFirstTestHook, AfterLastTestHook
+{
+    private array $fixtureLoaders = [];
+
+    public function executeBeforeFirstTest(): void
+    {
+        if ($this->getPhpUnitParameter("testsuite") !== 'integration test') {
+            return;
+        }
+
+        if (getenv('TESTS_LAMINAS_DB_ADAPTER_DRIVER_MYSQL')) {
+            $this->fixtureLoaders[] = new MysqlFixtureLoader();
+        }
+
+        if (getenv('TESTS_LAMINAS_DB_ADAPTER_DRIVER_PGSQL')) {
+            $this->fixtureLoaders[] = new PgsqlFixtureLoader();
+        }
+
+        if (getenv('TESTS_LAMINAS_DB_ADAPTER_DRIVER_SQLSRV')) {
+            $this->fixtureLoaders[] = new SqlServerFixtureLoader();
+        }
+
+        if (empty($this->fixtureLoaders)) {
+            return;
+        }
+
+        printf("\nIntegration test started.\n");
+
+        foreach ($this->fixtureLoaders as $fixtureLoader) {
+            $fixtureLoader->createDatabase();
+        }
+    }
+
+    public function executeAfterLastTest(): void
+    {
+        if (
+            $this->getPhpUnitParameter("testsuite") === 'integration test' ||
+            $this->fixtureLoaders === []
+        ) {
+            return;
+        }
+
+        printf("\nIntegration test ended.\n");
+
+        foreach ($this->fixtureLoaders as $fixtureLoader) {
+            $fixtureLoader->dropDatabase();
+        }
+    }
+
+    /**
+     * Resolves the parameters passed in to PHPUnit.
+     *
+     * eg. "phpunit --testsuite Unit --filter FirstTest"
+     *
+     * $this->getPhpUnitParameter("filter"); // Unit
+     * $this->getPhpUnitParameter("testsuite"); // FirstTest
+     */
+    private function getPhpUnitParameter(string $paramName): ?string
+    {
+        global $argv;
+
+        if ($offset = array_search("--$paramName", $argv) === false) {
+            return null;
+        }
+
+        return trim($argv[$offset + 1]);
+    }
+}
