de694f08fb60cf88959b4544708b879c9060851f
Abdul Malik Ikhsan <samsonasik@gmail.com>
2026-05-02 19:53:09 +0700

    use pdo_sqlsrv for php 8.5
    Signed-off-by: Abdul Malik Ikhsan <samsonasik@gmail.com>

--- .laminas-ci/pre-install.sh
+++ .laminas-ci/pre-install.sh
@@ -4,6 +4,18 @@ WORKING_DIRECTORY=$2
 JOB=$3
 PHP_VERSION=$(echo "${JOB}" | jq -r '.php')
 
+if [[ "${PHP_VERSION}" == "8.5" ]]; then
+  ACCEPT_EULA=Y apt update -qq
+  ACCEPT_EULA=Y apt install -yqq msodbcsql18 php-pear "php${PHP_VERSION}-dev" "php${PHP_VERSION}-xml" unixodbc-dev
+  pecl channel-update pecl.php.net
+  pecl install sqlsrv pdo_sqlsrv
+  printf "; priority=20\nextension=sqlsrv.so\n" > "/etc/php/${PHP_VERSION}/mods-available/sqlsrv.ini"
+  printf "; priority=30\nextension=pdo_sqlsrv.so\n" > "/etc/php/${PHP_VERSION}/mods-available/pdo_sqlsrv.ini"
+  phpenmod -v "${PHP_VERSION}" sqlsrv pdo_sqlsrv
+else
+  apt update -qq
+  apt install -yqq "php${PHP_VERSION}-sqlsrv"
+fi
 
 if [ ! -z "$GITHUB_BASE_REF" ] && [[ "$GITHUB_BASE_REF" =~ ^[0-9]+\.[0-9] ]]; then
   readarray -td. TARGET_BRANCH_VERSION_PARTS <<<"${GITHUB_BASE_REF}.";
